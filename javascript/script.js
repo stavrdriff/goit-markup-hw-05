2f2706e280e29376098f0eeb7fb63d370b9243a2
Tymofii <tymofii.wga@gmail.com>
2023-10-24 22:29:30 +0300

    shit

--- javascript/script.js
+++ javascript/script.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   
   function initModalTriggers() {
     const buttonsArr = [...document.querySelectorAll('[data-action="modalTrigger"]')];
-    const modalActiveSelector = 'show';
+    const modalActiveSelector = 'is-open';
 
     if (!buttonsArr.length) {
       return;
-    } 
+    }
 
     buttonsArr.forEach((button, index) => {
       button.addEventListener('click', () => {
@@ -22,6 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!modals.length) { 
       return;
     }
+
+    const body = document.querySelector('body');
+
+    body.classList.toggle('is-blocked')
     
     modals.forEach((modal, i) => {
       modal.classList.remove(modalActiveSelector);
@@ -40,20 +44,38 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     modals.forEach((modal) => {
-      const interactiveElements = [...modal.querySelectorAll('input, button')];
+      const interactiveElements = [...modal.querySelectorAll('input, button, a')];
+      const allInteractiveElements = [...document.querySelectorAll('input, button, a')];
+      let pageInteractiveElements = [];
 
-      tabindexHandler(interactiveElements, modalActiveSelector)    
+      allInteractiveElements.forEach((el) => {
+        if (!el.closest('.modal')) { 
+          pageInteractiveElements.push(el);
+        }
+      })
+
+      tabindexHandler(interactiveElements, modalActiveSelector,pageInteractiveElements)    
     })
   }
 
-  function tabindexHandler(elements, modalActiveSelector) {
+  function tabindexHandler(elements, modalActiveSelector, pageInteractiveElements) {
     elements.forEach((element) => {
       if (!element.closest(`.${modalActiveSelector}`)) {
         element.setAttribute('tabindex', '-1');
+
+        pageInteractiveElements.forEach((element) => {
+          element.setAttribute('tabindex', '0');
+        })
       } else { 
         element.setAttribute('tabindex', '0');
+
+        pageInteractiveElements.forEach((element) => {
+          element.setAttribute('tabindex', '-1');
+        })
       }
     })
+
+
   }
 
 
